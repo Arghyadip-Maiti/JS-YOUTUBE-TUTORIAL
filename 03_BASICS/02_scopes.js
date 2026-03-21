82fe455291928aac4d26ff6fb8621557b37308df
Arghyadip Maiti <arghyadipmaiti44@gmail.com>
2026-03-21 14:55:23 +0000

    scopes

---
+++ 03_BASICS/02_scopes.js
@@ -0,0 +1,46 @@
+//let, const, var
+
+//let-->Block Scope
+//const-->Block Scope
+//var-->Functional Scope
+
+
+function one(){
+    const name = "Arghya"
+
+    function two(){
+        const app = "youtube"
+        console.log(username);//'Arghya'
+    }
+    // console.log(app);//error
+    two()
+}
+one()
+
+
+if(true){
+    const name = "Arghya"
+    if(name==="Arghya") {
+        const app="youtube"
+        console.log(username + app);//Arghya youtube
+    }
+    // console.log(website);//error
+}
+// console.log(username);//error
+
+
+//+++++++++++++++++++ interesting ++++++++++++++++++++++++
+
+
+console.log(addone(5))//6
+
+function addone(num){
+    return num+1
+}
+
+
+addTwo(5)//TDZ(error)
+
+const addTwo = function(num){
+    return num + 2
+}
