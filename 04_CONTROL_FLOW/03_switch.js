a7ccf66e77c12e41112159d891686dcbc56ef7e9
Arghyadip Maiti <arghyadipmaiti44@gmail.com>
2026-03-24 17:57:38 +0000

    switch case

---
+++ 04_CONTROL_FLOW/03_switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value1: statement1; break;
+//     case value2: statement2; break;
+//     default: default-statement; break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
