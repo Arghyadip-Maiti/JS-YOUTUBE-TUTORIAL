f21306d96846b47ca4abaaffd1d938064b7620c3
Arghyadip Maiti <arghyadipmaiti44@gmail.com>
2026-03-25 05:57:53 +0000

    do-while loop

---
+++ 05_ITERATIONS/02_do_while_loop.js
@@ -0,0 +1,31 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+let score = 1
+
+do {
+    console.log(score);
+    score++;
+} while (score <= 10);// 1 \n 2 \n 3 \n 4 \n 5 \n 6 \n 7 \n 8 \n 9 \n 10
+
+let score2 = 11
+
+do {
+    console.log(score2);
+    score2++;
+} while (score2 <= 10);// 11
