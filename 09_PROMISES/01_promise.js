463c182079c5974ea234a119eb8ee41f84b7cd6e
Arghyadip Maiti <arghyadipmaiti44@gmail.com>
2026-04-03 06:51:18 +0000

    promise

---
+++ 09_PROMISES/01_promise.js
@@ -0,0 +1,126 @@
+//Initializing a promise...
+
+//let promiseName = new Promise((resolve,reject)=>{
+//    setTimeout(()=>{console.log('ADM');resolve()},1000);
+//    reject();//...can't use resolve and reject in one function...use only one...
+//});
+
+//promiseName.then(()=>{
+//    console.log('resolve');
+//})
+
+// promiseName.catch(()=>{
+//     console.log('reject');
+// })
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=->
+
+// let promise_Name = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{console.log('ADM');reject()},1000);
+// });
+
+// promise_Name.then(()=>{
+//     console.log('resolve');
+// })
+
+// promise_Name.catch(()=>{
+//     console.log('reject');
+// })
+
+// in case of vs code the above code will work but in node this will not work(node sees that there is nothing for the .then function so it throws an error...)
+
+// for node-->
+
+// let promiseName2 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{console.log('ADM');reject()},1000);
+// });
+
+// promiseName2.then(()=>{
+//     console.log('resolve');
+// }).catch(()=>{
+//     console.log('reject');
+// })
+
+
+//-----------------> modern syntax ----->
+
+// new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log(1);resolve();
+//     },1000);
+// }).then(()=>{
+//     console.log(2);
+// }).catch(()=>{
+//     console.log(3);
+// })
+
+
+
+// new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log(1);reject();resolve();
+//     },1000);
+// }).then(()=>{
+//     console.log(2);
+// }).catch(()=>{
+//     console.log(3);
+// })   // if both resolve and reject is there then what come first will only execute... in this case 1 \n 3
+
+
+//  passing data(mostly object) through resolve() and reject() --->
+
+
+// new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve({name:"ADM",age: 20 ,email: 'adm@gmail.com'});
+//         reject('asd');
+        
+//     },1000)
+// }).then((data)=>{
+//     console.log(data.age);
+// }).catch((str)=>{
+//     console.log(str);
+// })  // -> 20 //the reject function will be ignored as it's written later... 
+
+
+
+// chaining of .then()...--->
+
+//what you return from first then will be taken by the second then..
+
+
+// const promise = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         let error = false;
+
+//         if(!error){
+//             resolve({username: 'ADM', age: 20, gender: 'male'})
+//         }
+//         else{
+//             reject('404 error');
+//         }
+        
+//     },1000)
+// })    
+
+// promise.then((data)=>{
+//     console.log(data);
+//     return data.age;
+// }).then((age)=>{
+//     console.log(age);
+// }).catch((msg)=>{
+//     console.log(msg);
+// }).finally(()=>{
+//     console.log('lorem'); //it'll be executed no matter what...
+// })    
+
+
+//  getting the promise through async-await ---->
+
+// (async-await doesn't handle catch )
+
+
+
+
+
+
