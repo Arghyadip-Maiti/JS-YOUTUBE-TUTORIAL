7cc2504b7cc8448e3380c87e5664afd8edf676a3
Arghyadip Maiti <arghyadipmaiti44@gmail.com>
2026-04-04 14:23:08 +0000

    promise

--- 09_PROMISES/01_promise.js
+++ 09_PROMISES/01_promise.js
@@ -120,7 +120,99 @@
 // (async-await doesn't handle catch )
 
 
+// const promiseFive = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         let error = false;
+//         if(!error){
+//             resolve({userName: 'ADM', password: 123})
+//         }
+//         else{
+//             reject('404 not found')
+//         }
+//     },1000)
+// })
 
 
+// async function consumePromiseFive(){
+//     const response = await promiseFive //promise is an object...we consume promise with only name(referance) not like [ promiseFive() ]
+//     console.log(response);
+// }
+
+// consumePromiseFive();// if error is false then it will deal with resolve() and work perfectly....
+
+//-------------------------------->
+
+// const promiseFive = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         let error = true;
+//         if(!error){
+//             resolve({userName: 'ADM', password: 123})
+//         }
+//         else{
+//             reject('404 not found')
+//         }
+//     },1000)
+// })
 
 
+// async function consumePromiseFive(){
+//     const response = await promiseFive //promise is an object...we consume promise with only name(referance) not like [ promiseFive() ]
+//     console.log(response);
+// }
+
+// consumePromiseFive();// if error is true then it will deal with catch() and will not work perfectly....
+
+
+//to handle errors we'll use try-catch syntax in async-await ----------------->
+
+// const promiseFive = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         let error = true;
+//         if(!error){
+//             resolve({userName: 'ADM', password: 123})
+//         }
+//         else{
+//             reject('404 not found')
+//         }
+//     },1000)
+// })
+
+
+// async function consumePromiseFive(){
+//     try{
+//         const response = await promiseFive 
+//         console.log(response);
+//     }
+//     catch(error){
+//         console.log(error);
+//     }    
+// }
+
+// consumePromiseFive();// this will handle the error perfectly..
+
+
+// const asyncFunc = async()=>{
+//     try{
+//         let data = await fetch("https://jsonplaceholder.typicode.com/users");
+//         let jsonData = await data.json();
+//         console.log(jsonData);
+//     }
+//     catch(error){
+//         console.log("E:",error)
+//     }
+// }
+
+// asyncFunc();
+
+
+//  same thing in .catch() & .then()---->
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((data)=>{
+//     return data.json();
+// }).then((jsonData)=>{
+//     console.log(jsonData);
+// }).catch((error)=>{
+//     console.log(error);
+// })
